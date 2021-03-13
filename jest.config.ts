99484f5d78a1f55a81046fa19d6fffe29629ba02
brTomaz <bruno.tomaz@aluno.ufop.edu.br>
2021-03-13 13:09:05 -0300

    chore: add jest

---
+++ jest.config.ts
@@ -0,0 +1,11 @@
+export default {
+  clearMocks: true,
+  roots: ['<rootDir>/test/**/*.ts'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+  testEnvironment: 'node',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  }
+}
