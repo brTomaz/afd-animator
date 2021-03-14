8ebfed965d52967b64864359e2b58b29174ee385
brTomaz <bruno.tomaz@aluno.ufop.edu.br>
2021-03-14 19:10:05 -0300

    feat: add Automata

---
+++ src/entities/Automata.ts
@@ -0,0 +1,71 @@
+import State from './State';
+
+export default class Automata {
+  transitionFunction: Map<string, State>;
+
+  constructor(
+    private readonly initialState: State,
+    private readonly finalStates: State[]
+  ) {
+    this.transitionFunction = new Map();
+  }
+
+  getInitialState = (): State => {
+    return this.initialState;
+  };
+
+  getFinalStates = (): State[] => {
+    return this.finalStates;
+  };
+
+  process = (word: string) => {
+    let outputFileContent: string;
+    outputFileContent = 'strict digraph {';
+    let state = this.getInitialState().getName();
+
+    for (let iterator = 0; iterator < word.length; iterator++) {
+      let currentKey: string;
+      let prevState: string;
+
+      if (iterator === 0) {
+        prevState = this.getInitialState().getName();
+        currentKey = `${prevState},${word[iterator]}`;
+        state = this.transitionFunction.get(currentKey).getName();
+      } else {
+        prevState = state;
+        currentKey = `${state},${word[iterator]}`;
+        state = this.transitionFunction.get(currentKey).getName();
+      }
+      outputFileContent += `
+    ${prevState} -> ${state} [label=${word[iterator]}]`;
+    }
+
+    outputFileContent += this.createAutomataStructure() + '\n}';
+
+    let isWordAccept: boolean;
+
+    for (const s of this.finalStates) {
+      isWordAccept = s.getName() === state;
+      if (isWordAccept) break;
+    }
+    return {
+      isWordAccept,
+      outputFileContent,
+    };
+  };
+
+  createAutomataStructure = () => {
+    let structure = '';
+
+    this.transitionFunction.forEach((dest, key) => {
+      const keyFields = key.split(',');
+      const source = keyFields[0];
+      const symbol = keyFields[1];
+      const line = `
+      ${source} -> ${dest.getName()} [label=${symbol}]`;
+      structure += line;
+    });
+
+    return structure;
+  };
+}
