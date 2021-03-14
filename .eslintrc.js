0c3a06052d500d195a2492116d8c6c2e01ce6e99
brTomaz <bruno.tomaz@aluno.ufop.edu.br>
2021-03-14 20:15:03 -0300

    chore: add jest to eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,13 +3,13 @@ module.exports = {
     es2021: true,
     node: true,
   },
-  extends: ['standard', 'prettier'],
+  extends: ['standard', 'prettier', 'plugin:jest/recommended'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 12,
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint', 'prettier'],
+  plugins: ['@typescript-eslint', 'prettier', 'jest'],
   rules: {
     'no-useless-constructor': 'off',
     'prettier/prettier': 'error',
--- jest.config.ts
+++ jest.config.ts
@@ -1,11 +1,11 @@
 export default {
   clearMocks: true,
-  roots: ['<rootDir>/test/**/*.ts'],
+  roots: ['<rootDir>/test'],
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
   coverageDirectory: 'coverage',
   coverageProvider: 'v8',
   testEnvironment: 'node',
   transform: {
-    '.+\\.ts$': 'ts-jest'
-  }
-}
+    '.+\\.ts$': 'ts-jest',
+  },
+};
